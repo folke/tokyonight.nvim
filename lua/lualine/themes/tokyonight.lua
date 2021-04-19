3f4ae6e0b3bd940413fd95b8cc36b7ddaa0cd2bd
Folke Lemaitre <folke.lemaitre@gmail.com>
2021-04-19 20:42:20 +0200

    better lualine colors

--- lua/lualine/themes/tokyonight.lua
+++ lua/lualine/themes/tokyonight.lua
@@ -3,31 +3,37 @@ local colors = require("tokyonight.colors")
 local tokyonight = {}
 
 local c = { bg = colors.bg_statusline, fg = colors.fg_sidebar }
+
 tokyonight.normal = {
   a = { bg = colors.blue, fg = colors.bg_dark },
   b = { bg = colors.fg_gutter, fg = colors.blue },
   c = c,
 }
+
 tokyonight.insert = {
   a = { bg = colors.green3, fg = colors.bg_dark },
   b = { bg = colors.fg_gutter, fg = colors.green3 },
   c = c,
 }
+
 tokyonight.command = {
   a = { bg = colors.orange, fg = colors.bg_dark },
   b = { bg = colors.fg_gutter, fg = colors.orange },
   c = c,
 }
+
 tokyonight.visual = {
   a = { bg = colors.blue0, fg = colors.bg_dark },
   b = { bg = colors.fg_gutter, fg = colors.blue0 },
   c = c,
 }
+
 tokyonight.replace = {
   a = { bg = colors.red, fg = colors.bg_dark },
   b = { bg = colors.fg_gutter, fg = colors.red },
   c = c,
 }
+
 tokyonight.inactive = {
   a = { bg = colors.bg_statusline, fg = colors.blue },
   b = { bg = colors.bg_statusline, fg = colors.fg_gutter, gui = "bold" },
