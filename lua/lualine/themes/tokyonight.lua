666ebba21063381fced8ce0470431204569d531d
Folke Lemaitre <folke.lemaitre@gmail.com>
2021-04-20 18:00:27 +0200

    changed lualine VISUAL mode to magenta

--- lua/lualine/themes/tokyonight.lua
+++ lua/lualine/themes/tokyonight.lua
@@ -13,25 +13,21 @@ tokyonight.normal = {
 tokyonight.insert = {
   a = { bg = colors.green, fg = colors.bg_dark },
   b = { bg = colors.fg_gutter, fg = colors.green },
-  c = c,
 }
 
 tokyonight.command = {
   a = { bg = colors.yellow, fg = colors.bg_dark },
   b = { bg = colors.fg_gutter, fg = colors.yellow },
-  c = c,
 }
 
 tokyonight.visual = {
-  a = { bg = colors.blue0, fg = colors.fg },
-  b = { bg = colors.fg_gutter, fg = colors.fg },
-  c = c,
+  a = { bg = colors.magenta, fg = colors.bg_dark },
+  b = { bg = colors.fg_gutter, fg = colors.magenta },
 }
 
 tokyonight.replace = {
   a = { bg = colors.red, fg = colors.bg_dark },
   b = { bg = colors.fg_gutter, fg = colors.red },
-  c = c,
 }
 
 tokyonight.inactive = {
