3eecf159d051041806f6e56b5459c02e44048656
Folke Lemaitre <folke.lemaitre@gmail.com>
2021-04-20 20:49:14 +0200

    fix: small typo in def for Pmenu

--- lua/tokyonight/theme.lua
+++ lua/tokyonight/theme.lua
@@ -51,7 +51,7 @@ function M.setup(config)
     NormalSB = { fg = c.fg_sidebar, bg = c.bg_sidebar }, -- normal text in non-current windows
     NormalFloat = { fg = c.fg, bg = c.bg_float }, -- Normal text in floating windows.
     FloatBorder = { fg = c.border_highlight },
-    Pmenu = { bg = c.bg_popup, c.fg }, -- Popup menu: normal item.
+    Pmenu = { bg = c.bg_popup, fg = c.fg }, -- Popup menu: normal item.
     PmenuSel = { bg = util.darken(c.fg_gutter, 0.8) }, -- Popup menu: selected item.
     PmenuSbar = { bg = util.lighten(c.bg_popup, 0.95) }, -- Popup menu: scrollbar.
     PmenuThumb = { bg = c.fg_gutter }, -- Popup menu: Thumb of the scrollbar.
