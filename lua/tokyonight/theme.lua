65002095977abe2ff59b8e600c48b5013696304a
Folke Lemaitre <folke.lemaitre@gmail.com>
2021-04-21 08:04:57 +0200

    better tabline sel

--- lua/tokyonight/theme.lua
+++ lua/tokyonight/theme.lua
@@ -67,8 +67,8 @@ function M.setup(config)
     StatusLine = { fg = c.fg_sidebar, bg = c.bg_statusline }, -- status line of current window
     StatusLineNC = { fg = c.fg_sidebar, bg = c.bg_statusline }, -- status lines of not-current windows Note: if this is equal to "StatusLine" Vim will use "^^^" in the status line of the current window.
     TabLine = { bg = c.bg_statusline, fg = c.fg_gutter }, -- tab pages line, not active tab page label
-    TabLineFill = { bg = c.bg_statusline }, -- tab pages line, where there are no labels
-    TabLineSel = { fg = c.blue, bg = c.bg }, -- tab pages line, active tab page label
+    TabLineFill = { bg = c.black }, -- tab pages line, where there are no labels
+    TabLineSel = { fg = c.black, bg = c.blue }, -- tab pages line, active tab page label
     Title = { fg = c.blue, style = "bold" }, -- titles for output from ":set all", ":autocmd" etc.
     Visual = { bg = c.bg_visual }, -- Visual mode selection
     VisualNOS = { bg = c.bg_visual }, -- Visual mode selection when vim is "Not Owning the Selection".
