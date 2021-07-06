50b1e71be5ac05d03ad8dc1f776b747466b3d3cc
Folke Lemaitre <folke.lemaitre@gmail.com>
2021-07-06 16:40:18 +0200

    feat: added .Xresources

--- lua/tokyonight/extra/init.lua
+++ lua/tokyonight/extra/init.lua
@@ -3,23 +3,30 @@ package.path = "./lua/?/init.lua;./lua/?.lua"
 local config = require("tokyonight.config")
 
 local function write(str, fileName)
-  print("[write] extra/" .. fileName)
-  local file = io.open("extras/" .. fileName, "w")
-  file:write(str)
-  file:close()
+	print("[write] extra/" .. fileName)
+	local file = io.open("extras/" .. fileName, "w")
+	file:write(str)
+	file:close()
 end
 
 -- map of plugin name to plugin extension
-local extras = { kitty = "conf", fish = "fish", alacritty = "yml", wezterm = "toml", tmux = "tmux" }
+local extras = {
+	kitty = "conf",
+	fish = "fish",
+	alacritty = "yml",
+	wezterm = "toml",
+	tmux = "tmux",
+	xresources = "Xresources",
+}
 local styles = { "storm", "night", "day" }
 
 for extra, ext in pairs(extras) do
-  local plugin = require("tokyonight.extra." .. extra)
-  for _, style in pairs(styles) do
-    config.style = style
-    config = config or require("tokyonight.config")
-    config.transform_colors = true
-    local colors = require("tokyonight.colors").setup(config)
-    write(plugin.generate(colors), extra .. "_tokyonight_" .. style .. "." .. ext)
-  end
+	local plugin = require("tokyonight.extra." .. extra)
+	for _, style in pairs(styles) do
+		config.style = style
+		config = config or require("tokyonight.config")
+		config.transform_colors = true
+		local colors = require("tokyonight.colors").setup(config)
+		write(plugin.generate(colors), extra .. "_tokyonight_" .. style .. "." .. ext)
+	end
 end
