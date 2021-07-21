10992230ff4c68e53b133b18c849c09063a96014
ian mcgaunn <ian@mcgaunn.com>
2021-07-21 16:37:31 -0400

    feat: add colorscheme generator and themes for xfce4 terminal

---
+++ lua/tokyonight/extra/xfceterm.lua
@@ -0,0 +1,24 @@
+local util = require("tokyonight.util")
+
+local M = {}
+
+-- @param colors ColorScheme
+function M.generate(colors)
+	local xfceterm = util.template(
+		[[
+[Scheme]
+Name=TokyoNight Colors
+ColorBackground=${bg}
+ColorForeground=${fg}
+
+ColorSelectionBackground=${bg_visual}
+ColorSelection=${fg}
+
+ColorPalette=${black};${red};${green};${yellow};${blue};${magenta};${cyan};${fg_dark};${terminal_black};${red};${green};${yellow};${blue};${magenta};${cyan};${fg}
+]],
+		colors
+	)
+	return xfceterm
+end
+
+return M
