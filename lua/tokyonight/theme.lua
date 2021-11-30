0d917649551662f8cd389c26460d70949630353d
Folke Lemaitre <folke.lemaitre@gmail.com>
2021-11-30 09:06:50 +0100

    feat: added support for CmpKind hilight groups

--- lua/tokyonight/theme.lua
+++ lua/tokyonight/theme.lua
@@ -409,12 +409,26 @@ function M.setup(config)
     -- Cmp
     CmpDocumentation = { fg = c.fg, bg = c.bg_float },
     CmpDocumentationBorder = { fg = c.border_highlight, bg = c.bg_float },
+
     CmpItemAbbr = { fg = c.fg, bg = c.none },
-    CmpItemAbbrDeprecated = { fg = c.fg_gutter, bg = c.none },
-    CmpItemAbbrMatch = { fg = c.green1, bg = c.none },
-    CmpItemAbbrMatchFuzzy = { fg = c.green1, bg = c.none },
-    CmpItemKind = { fg = c.teal, bg = c.none },
+    CmpItemAbbrDeprecated = { fg = c.fg_gutter, bg = c.none, style = "strikethrough" },
+    CmpItemAbbrMatch = { fg = c.blue1, bg = c.none },
+    CmpItemAbbrMatchFuzzy = { fg = c.blue1, bg = c.none },
+
+    CmpItemKindDefault = { fg = c.fg_dark, bg = c.none },
     CmpItemMenu = { fg = c.comment, bg = c.none },
+
+    CmpItemKindVariable = { fg = c.magenta, bg = c.none },
+
+    CmpItemKindFunction = { fg = c.blue, bg = c.none },
+    CmpItemKindMethod = { fg = c.blue, bg = c.none },
+
+    CmpItemKindClass = { fg = c.orange, bg = c.none },
+    CmpItemKindInterface = { fg = c.orange, bg = c.none },
+
+    CmpItemKindProperty = { fg = c.green1, bg = c.none },
+    CmpItemKindField = { fg = c.green1, bg = c.none },
+    CmpItemKindSnippet = { fg = c.dark5, bg = c.none },
   }
 
   theme.defer = {}
