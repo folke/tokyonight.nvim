eb6803854a4b076d110d13cd66f5472f7abff15c
Hass <hass.khalid@agilebits.com>
2022-09-02 06:53:01 +0100

    fix: Improved telescope theme for non transparent mode. (#146)
    * set TelescopeNormal to default fg and bg colors
    
    When transparency is enabled the background for Telescope seems to be
    just black. This this doesn't blend well with the rest of the theme in
    transparency mode. While the default fg and bg are not transparent, they
    do provide a better contrast and seems easier on the eye.
    
    * Telescope background defaults to none if transparent not set

--- lua/tokyonight/theme.lua
+++ lua/tokyonight/theme.lua
@@ -306,8 +306,8 @@ function M.setup(config)
     GitSignsDelete = { fg = c.gitSigns.delete }, -- diff mode: Deleted line |diff.txt|
 
     -- Telescope
-    TelescopeBorder = { fg = c.border_highlight, bg = c.bg_float },
-    TelescopeNormal = { fg = c.fg, bg = c.bg_float },
+    TelescopeBorder = { fg = c.border_highlight, bg = config.transparent and c.bg_float or c.none },
+    TelescopeNormal = { fg = c.fg, bg = config.transparent and c.bg_float or c.none },
 
     -- NvimTree
     NvimTreeNormal = { fg = c.fg_sidebar, bg = c.bg_sidebar },
