df281cc0f1a277b6150df24feb809e86f57c2374
Loong Wang <wangl.cc@outlook.com>
2022-10-03 00:25:44 +0800

    feat: add options light_style (#231)
    * feat: add options light_style
    
    * add default and docs for light_style

--- lua/tokyonight/config.lua
+++ lua/tokyonight/config.lua
@@ -5,6 +5,7 @@ local M = {}
 ---@field on_highlights fun(highlights: Highlights, colors: ColorScheme)
 local defaults = {
   style = "storm", -- The theme comes in three styles, `storm`, a darker variant `night` and `day`
+  light_style = "day", -- The theme is used when the background is set to light
   transparent = false, -- Enable this to disable setting the background color
   terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
   styles = {
