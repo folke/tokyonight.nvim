6572022433fd4dde3af16cd5be2b93ce6b6a6ef3
Folke Lemaitre <folke.lemaitre@gmail.com>
2022-11-10 18:17:53 +0100

    feat: better highlight style for delta

--- lua/tokyonight/extra/delta.lua
+++ lua/tokyonight/extra/delta.lua
@@ -4,16 +4,20 @@ local M = {}
 
 --- @param colors ColorScheme
 function M.generate(colors)
+  colors.delta = {
+    add = util.darken(colors.green2, 0.45),
+    delete = util.darken(colors.red1, 0.45),
+  }
   local delta = util.template(
     [[
 [delta]
 minus-style                   = syntax "${diff.delete}"
 minus-non-emph-style          = syntax "${diff.delete}"
-minus-emph-style              = "${black}" "${red1}"
+minus-emph-style              = syntax "${delta.delete}"
 minus-empty-line-marker-style = syntax "${diff.delete}"
 plus-style                    = syntax "${diff.add}"
 plus-non-emph-style           = syntax "${diff.add}"
-plus-emph-style               = "${black}" "${green2}"
+plus-emph-style               = syntax "${delta.add}"
 plus-empty-line-marker-style  = syntax "${diff.add}"
 ]],
     colors
