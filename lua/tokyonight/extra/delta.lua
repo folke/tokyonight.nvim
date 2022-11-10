7e15cff51d6099d88eac2fd2b5cb825ab867da76
Folke Lemaitre <folke.lemaitre@gmail.com>
2022-11-10 18:21:41 +0100

    feat: delta line number styles

--- lua/tokyonight/extra/delta.lua
+++ lua/tokyonight/extra/delta.lua
@@ -11,14 +11,17 @@ function M.generate(colors)
   local delta = util.template(
     [[
 [delta]
-minus-style                   = syntax "${diff.delete}"
-minus-non-emph-style          = syntax "${diff.delete}"
-minus-emph-style              = syntax "${delta.delete}"
-minus-empty-line-marker-style = syntax "${diff.delete}"
-plus-style                    = syntax "${diff.add}"
-plus-non-emph-style           = syntax "${diff.add}"
-plus-emph-style               = syntax "${delta.add}"
-plus-empty-line-marker-style  = syntax "${diff.add}"
+  minus-style                   = syntax "${diff.delete}"
+  minus-non-emph-style          = syntax "${diff.delete}"
+  minus-emph-style              = syntax "${delta.delete}"
+  minus-empty-line-marker-style = syntax "${diff.delete}"
+  line-numbers-minus-style      = "${gitSigns.delete}"
+  plus-style                    = syntax "${diff.add}"
+  plus-non-emph-style           = syntax "${diff.add}"
+  plus-emph-style               = syntax "${delta.add}"
+  plus-empty-line-marker-style  = syntax "${diff.add}"
+  line-numbers-plus-style       = "${gitSigns.add}"
+  line-numbers-zero-style       = "${fg_gutter}"
 ]],
     colors
   )
