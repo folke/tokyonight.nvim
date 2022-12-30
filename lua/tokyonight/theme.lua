3f565694351ac9534dc427a11afc41190f9dee40
Folke Lemaitre <folke.lemaitre@gmail.com>
2022-12-30 11:51:27 +0100

    feat: added helpCommand

--- lua/tokyonight/theme.lua
+++ lua/tokyonight/theme.lua
@@ -167,6 +167,7 @@ function M.setup()
     ["@text.todo.checked"] = { fg = c.green1 }, -- For brackets and parens.
     ["@text.literal.markdown_inline"] = { bg = c.terminal_black, fg = c.blue },
     ["@text.literal.markdown"] = { link = "Normal" },
+    ["helpCommand"] = { bg = c.terminal_black, fg = c.blue },
 
     debugPC = { bg = c.bg_sidebar }, -- used for highlighting the current line in terminal-debug
     debugBreakpoint = { bg = util.darken(c.info, 0.1), fg = c.info }, -- used for breakpoint colors in terminal-debug
