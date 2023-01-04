d785bffce2b5f5a8e8ff9b4f2e4fb3ce7d469b3e
rishabhjain9191 <rishabh.jain9191@gmail.com>
2023-01-04 21:43:07 +0530

    docs: update config.lua (#292)
    Fix typos in comments

--- lua/tokyonight/config.lua
+++ lua/tokyonight/config.lua
@@ -26,12 +26,12 @@ local defaults = {
   lualine_bold = false, -- When `true`, section headers in the lualine theme will be bold
 
   --- You can override specific color groups to use other groups or a hex color
-  --- fucntion will be called with a ColorScheme table
+  --- function will be called with a ColorScheme table
   ---@param colors ColorScheme
   on_colors = function(colors) end,
 
   --- You can override specific highlights to use other groups or a hex color
-  --- fucntion will be called with a Highlights and ColorScheme table
+  --- function will be called with a Highlights and ColorScheme table
   ---@param highlights Highlights
   ---@param colors ColorScheme
   on_highlights = function(highlights, colors) end,
