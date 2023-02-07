56945bd0d312dc3ed84466d7a6cbfc5e44fbbb4e
Folke Lemaitre <folke.lemaitre@gmail.com>
2023-02-07 21:30:08 +0100

    feat(wezterm): added tab_bar colors. only works with the retro tab bar. Fixes #304

--- lua/tokyonight/extra/wezterm.lua
+++ lua/tokyonight/extra/wezterm.lua
@@ -17,7 +17,31 @@ selection_fg = "${fg}"
 
 ansi = ["${black}", "${red}", "${green}", "${yellow}", "${blue}", "${magenta}", "${cyan}", "${fg_dark}"]
 brights = ["${terminal_black}", "${red}", "${green}", "${yellow}", "${blue}", "${magenta}", "${cyan}", "${fg}"]
-  ]],
+
+[colors.tab_bar]
+inactive_tab_edge = "${bg_dark}"
+background = "#191b28"
+
+[colors.tab_bar.active_tab]
+fg_color = "${blue}"
+bg_color = "${bg}"
+
+[colors.tab_bar.inactive_tab]
+bg_color = "${bg_dark}"
+fg_color = "${dark3}"
+
+[colors.tab_bar.inactive_tab_hover]
+bg_color = "${bg_dark}"
+fg_color = "${blue}"
+
+[colors.tab_bar.new_tab_hover]
+fg_color = "${bg_dark}"
+bg_color = "${blue}"
+
+[colors.tab_bar.new_tab]
+fg_color = "${blue}"
+bg_color = "#191b28"
+]],
     colors
   )
   return kitty
