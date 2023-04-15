06fa42cf7d15ae5378e827bd645573fc3060508d
Yuxin Wu <ppwwyyxxc@gmail.com>
2023-04-15 00:54:24 -0700

    Add highlight of GitGutter**LineNr

--- lua/tokyonight/theme.lua
+++ lua/tokyonight/theme.lua
@@ -343,6 +343,9 @@ function M.setup()
     GitGutterAdd = { fg = c.gitSigns.add }, -- diff mode: Added line |diff.txt|
     GitGutterChange = { fg = c.gitSigns.change }, -- diff mode: Changed line |diff.txt|
     GitGutterDelete = { fg = c.gitSigns.delete }, -- diff mode: Deleted line |diff.txt|
+    GitGutterAddLineNr = { fg = c.gitSigns.add },
+    GitGutterChangeLineNr = { fg = c.gitSigns.change },
+    GitGutterDeleteLineNr = { fg = c.gitSigns.delete },
 
     -- GitSigns
     GitSignsAdd = { fg = c.gitSigns.add }, -- diff mode: Added line |diff.txt|
