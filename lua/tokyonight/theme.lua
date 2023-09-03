b4da1c33759bfd43d059adb09c67069a87ec73aa
Zeioth <zeioth@hotmail.com>
2023-09-03 21:08:08 +0200

    feat(new markdown notes)

--- lua/tokyonight/theme.lua
+++ lua/tokyonight/theme.lua
@@ -224,7 +224,7 @@ function M.setup()
     --- Functions
     ["@constructor"] = { fg = c.magenta }, -- For constructor calls and definitions: `= { }` in Lua, and Java constructors.
     ["@parameter"] = { fg = c.yellow }, -- For parameters of a function.
-    ["@parameter.builtin"] = { fg = "#efc890" }, -- For builtin parameters of a function, e.g. "..." or Smali's p[1-99]
+    ["@parameter.builtin"] = { fg = "#efc890" },
 
     -- TODO:
     -- ["@parameter.builtin"] = {}, -- For builtin parameters of a function, e.g. "..." or Smali's p[1-99]
@@ -806,9 +806,14 @@ function M.setup()
     callback = function()
       if vim.bo.filetype == "markdown" then
         vim.defer_fn(function()
+          -- TASK:
+          vim.cmd(":silent! highlight clear MarkdownTask")
+          vim.cmd(":highlight MarkdownTask guifg=" .. c.teal .. " gui=bold")
+          vim.cmd(":syntax match MarkdownTask /\\cTask:/")
+
           -- TODO:
           vim.cmd(":silent! highlight clear MarkdownTodo")
-          vim.cmd(":highlight MarkdownTodo guifg=" .. c.teal .. " gui=bold")
+          vim.cmd(":highlight MarkdownTodo guifg=" .. c.yellow .. " gui=bold")
           vim.cmd(":syntax match MarkdownTodo /\\cTODO:/")
 
           -- NOTE:
@@ -830,6 +835,11 @@ function M.setup()
           vim.cmd(":silent! highlight clear MarkdownURL")
           vim.cmd(":highlight MarkdownURL guifg=" .. c.purple .. " gui=bold")
           vim.cmd(":syntax match MarkdownURL /\\cURL:/")
+
+          -- EXAMPLE:
+          vim.cmd(":silent! highlight clear MarkdownExample")
+          vim.cmd(":highlight MarkdownExample guifg=" .. c.magenta .. " gui=bold")
+          vim.cmd(":syntax match MarkdownExample /\\cExample:/")
         end, 100)
       end
     end,
