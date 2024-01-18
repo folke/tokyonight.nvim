c9abf0b9752dbf1f823edf34240042928014d85f
Sergey Kochetkov <skoch13@icloud.com>
2024-01-18 20:53:34 +0100

    Update lua/tokyonight/extra/fzf.lua
    Co-authored-by: Şahin Akkaya <sahin@sahinakkaya.dev>

--- lua/tokyonight/extra/fzf.lua
+++ lua/tokyonight/extra/fzf.lua
@@ -9,7 +9,7 @@ end
 
 M.template = [[
 export FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS \
---color=fg:${fg},bg:${bg},hl:${orange} \ 
+--color=fg:${fg},bg:${bg},hl:${orange} \
 --color=fg+:${fg},bg+:${bg_highlight},hl+:${orange} \
 --color=info:${blue},prompt:${cyan},pointer:${cyan} \
 --color=marker:${green},spinner:${green},header:${green}"
