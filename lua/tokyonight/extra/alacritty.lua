ffc7505a1426ec2472c75d8e8712df611a5ef5a2
Joseph Sebastian <josephsv96@gmail.com>
2024-01-20 12:17:47 +0100

    feat: changed alacritty .yml to .toml (#450)
    * feat: changed alacritty .yml to .toml
    
    * feat: using hex colors for alacritty
    
    * added *toml, removed *.yml

--- lua/tokyonight/extra/alacritty.lua
+++ lua/tokyonight/extra/alacritty.lua
@@ -4,50 +4,51 @@ local M = {}
 
 --- @param colors ColorScheme
 function M.generate(colors)
-  local alacrittyColors = {}
-  for k, v in pairs(colors) do
-    if type(v) == "string" then
-      alacrittyColors[k] = v:gsub("^#", "0x")
-    end
-  end
-
   local alacritty = util.template(
-    [[
+    [=[
 # TokyoNight Alacritty Colors
-colors:
-  # Default colors
-  primary:
-    background: '${bg}'
-    foreground: '${fg}'
+# Default colors
+[colors.primary]
+background = '${bg}'
+foreground = '${fg}'
 
-  # Normal colors
-  normal:
-    black:   '${black}'
-    red:     '${red}'
-    green:   '${green}'
-    yellow:  '${yellow}'
-    blue:    '${blue}'
-    magenta: '${magenta}'
-    cyan:    '${cyan}'
-    white:   '${fg_dark}'
+#[colors.cursor]
+#cursor = '${fg}'
+#text = '${bg}'
 
-  # Bright colors
-  bright:
-    black:   '${terminal_black}'
-    red:     '${red}'
-    green:   '${green}'
-    yellow:  '${yellow}'
-    blue:    '${blue}'
-    magenta: '${magenta}'
-    cyan:    '${cyan}'
-    white:   '${fg}'
+# Normal colors
+[colors.normal]
+black = '${black}'
+red = '${red}'
+green = '${green}'
+yellow = '${yellow}'
+blue = '${blue}'
+magenta = '${magenta}'
+cyan = '${cyan}'
+white = '${fg_dark}'
 
-  indexed_colors:
-    - { index: 16, color: '${orange}' }
-    - { index: 17, color: '${red1}' }
-    
-  ]],
-    alacrittyColors
+# Bright colors
+[colors.bright]
+black = '${terminal_black}'
+red = '${red}'
+green = '${green}'
+yellow = '${yellow}'
+blue = '${blue}'
+magenta = '${magenta}'
+cyan = '${cyan}'
+white = '${fg}'
+
+# Indexed Colors
+[[colors.indexed_colors]]
+index = 16
+color = '${orange}'
+
+[[colors.indexed_colors]]
+index = 17
+color = '${red1}'
+
+]=],
+    colors
   )
 
   return alacritty
