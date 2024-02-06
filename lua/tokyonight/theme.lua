34f1efef419c7e0ee7a1df015d70f88337a628aa
gi1242 <gi1242+tokyonight@gmail.com>
2024-02-05 20:07:53 -0500

    fix(day)!: Chose palette for day
    BREAKING CHANGE: removed day_brightness, as it's not needed anymore

--- lua/tokyonight/theme.lua
+++ lua/tokyonight/theme.lua
@@ -872,11 +872,6 @@ function M.setup()
 
   options.on_highlights(theme.highlights, theme.colors)
 
-  if config.is_day() then
-    util.invert_colors(theme.colors)
-    util.invert_highlights(theme.highlights)
-  end
-
   return theme
 end
 
