29b64d5523965d4c6612f85d13b6eb20a6c16c9f
gi1242 <gi1242+tokyonight@gmail.com>
2024-02-05 20:07:53 -0500

    fix(day)!: Chose palette for day
    BREAKING CHANGE: removed day_brightness, as it's not needed anymore

--- lua/tokyonight/config.lua
+++ lua/tokyonight/config.lua
@@ -20,7 +20,6 @@ local defaults = {
     floats = "dark", -- style for floating windows
   },
   sidebars = { "qf", "help" }, -- Set a darker background on sidebar-like windows. For example: `["qf", "vista_kind", "terminal", "packer"]`
-  day_brightness = 0.3, -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
   hide_inactive_statusline = false, -- Enabling this option, will hide inactive statuslines and replace them with a thin border instead. Should work with the standard **StatusLine** and **LuaLine**.
   dim_inactive = false, -- dims inactive windows
   lualine_bold = false, -- When `true`, section headers in the lualine theme will be bold
