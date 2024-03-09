96237c77217207e94bf92aa58284980e3ce4dc31
rizqicy <dev@rizqi.cy>
2024-03-09 14:46:19 +0700

    feat(extra): add aerc mail client theme

--- lua/tokyonight/extra/init.lua
+++ lua/tokyonight/extra/init.lua
@@ -26,6 +26,7 @@ M.extras = {
   gitui = {ext = "ron", url = "https://github.com/extrawurst/gitui", label = "GitUI"},
   helix = { ext = "toml", url = "https://helix-editor.com/", label = "Helix"},
   fzf = { ext = "zsh", url = "https://github.com/junegunn/fzf", label = "Fzf"},
+  aerc = { ext = "ini", url = "https://git.sr.ht/~rjarry/aerc/", label = "Aerc"},
 }
 
 local function write(str, fileName)
