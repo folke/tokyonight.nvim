770cd5ed4ee7ce7e51fd596f376f578e1bf6399f
David <daidavid0609@gmail.com>
2024-05-25 22:35:25 +0200

    Changed inlay hint fg to use dark5

--- lua/tokyonight/theme.lua
+++ lua/tokyonight/theme.lua
@@ -200,7 +200,7 @@ function M.setup()
 
     LspSignatureActiveParameter = { bg = util.darken(c.bg_visual, 0.4), bold = true },
     LspCodeLens = { fg = c.comment },
-    LspInlayHint = { bg = util.darken(c.blue7, 0.1), fg = c.dark3 },
+    LspInlayHint = { bg = util.darken(c.blue7, 0.1), fg = c.dark5 },
 
     LspInfoBorder = { fg = c.border_highlight, bg = c.bg_float },
 
