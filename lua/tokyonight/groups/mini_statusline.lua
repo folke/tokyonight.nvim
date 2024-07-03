29e9fce3184c534936897dfae537e9aa10c280b1
231tr0n <zeltronsrikar@gmail.com>
2024-07-03 12:16:49 +0530

    fix(mini-satusline): reverse colors of devinfo and fileinfo with filename to make the colors look like lualine for mini.statusline (#564)

--- lua/tokyonight/groups/mini_statusline.lua
+++ lua/tokyonight/groups/mini_statusline.lua
@@ -6,9 +6,9 @@ M.url = "https://github.com/echasnovski/mini.statusline"
 function M.get(c)
   -- stylua: ignore
   return {
-    MiniStatuslineDevinfo     = { fg = c.fg_dark, bg = c.bg_highlight },
-    MiniStatuslineFileinfo    = { fg = c.fg_dark, bg = c.bg_highlight },
-    MiniStatuslineFilename    = { fg = c.fg_dark, bg = c.fg_gutter },
+    MiniStatuslineDevinfo     = { fg = c.fg_dark, bg = c.fg_gutter },
+    MiniStatuslineFileinfo    = { fg = c.fg_dark, bg = c.fg_gutter },
+    MiniStatuslineFilename    = { fg = c.fg_dark, bg = c.bg_highlight },
     MiniStatuslineInactive    = { fg = c.blue, bg = c.bg_statusline },
     MiniStatuslineModeCommand = { fg = c.black, bg = c.yellow, bold = true },
     MiniStatuslineModeInsert  = { fg = c.black, bg = c.green, bold = true },
