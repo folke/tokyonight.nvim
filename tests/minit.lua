86ee7eb3bec80a3f90c535069297eb6488c12133
Folke Lemaitre <folke.lemaitre@gmail.com>
2024-07-05 15:13:06 +0200

    ci: trigger build

--- tests/minit.lua
+++ tests/minit.lua
@@ -4,7 +4,7 @@ vim.env.LAZY_STDPATH = ".tests"
 vim.env.LAZY_PATH = vim.fs.normalize("~/projects/lazy.nvim")
 load(vim.fn.system("curl -s https://raw.githubusercontent.com/folke/lazy.nvim/main/bootstrap.lua"))()
 
--- Setup lazy.nvim
+-- Setup lazy
 require("lazy.minit").setup({
   spec = {
     {
