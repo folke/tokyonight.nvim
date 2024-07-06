b6e672726fa5d879a6b1b55dab6c31d60bd032d4
Kirill <ibotlold@gmail.com>
2024-07-06 14:22:35 +0500

    fix: use `LeapLabel`
    `LeapLabelPrimary`, `LeapLabelSecondary` deprecated

--- lua/tokyonight/groups/leap.lua
+++ lua/tokyonight/groups/leap.lua
@@ -7,8 +7,7 @@ function M.get(c, opts)
   -- stylua: ignore
   return {
     LeapMatch          = { bg = c.magenta2, fg = c.fg, bold = true },
-    LeapLabelPrimary   = { fg = c.magenta2, bold = true },
-    LeapLabelSecondary = { fg = c.green1, bold = true },
+    LeapLabel          = { fg = c.magenta2, bold = true },
     LeapBackdrop       = { fg = c.dark3 },
   }
 end
