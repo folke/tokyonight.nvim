25556d1778573d3581993318eb09ba12a7ae28b4
folke <folke@users.noreply.github.com>
2024-07-08 05:15:33 +0000

    chore(build): auto-generated build files

--- extras/fish/tokyonight_moon.fish
+++ extras/fish/tokyonight_moon.fish
@@ -17,6 +17,7 @@
     set -g fish_color_quote $yellow
     set -g fish_color_redirection $foreground
     set -g fish_color_end $orange
+    set -g fish_color_option $orange
     set -g fish_color_error $red
     set -g fish_color_param $purple
     set -g fish_color_comment $comment
