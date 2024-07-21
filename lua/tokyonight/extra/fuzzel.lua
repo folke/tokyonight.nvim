c48728779e64eaccd8414201883f3953a66ec7bc
lemonknife <lemonife@protonmail.com>
2024-07-22 00:27:24 +0800

    fix(extras): use PmenuSel as selected bg for fuzzel

--- lua/tokyonight/extra/fuzzel.lua
+++ lua/tokyonight/extra/fuzzel.lua
@@ -5,18 +5,20 @@ local M = {}
 --- @param colors ColorScheme
 function M.generate(colors)
   local fuzzelColors = {}
+  colors.bg_search = util.blend_bg(colors.fg_gutter, 0.8)
   for k, v in pairs(colors) do
     if type(v) == "string" then
       fuzzelColors[k] = v:gsub("^#", "") .. "ff"
     end
   end
+
   local fuzzel = util.template(
     [[
 [colors]
 background=${bg_popup}
 text=${fg}
 match=${blue1}
-selection=${bg_highlight}
+selection=${bg_search}
 selection-match=${blue1}
 selection-text=${fg}
 border=${border_highlight}
