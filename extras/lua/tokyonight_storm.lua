9699675f988ad5a2df21b7875362af6d15d0f53c
Christoph Zirkelbach <31811+tigion@users.noreply.github.com>
2024-09-16 08:43:20 +0200

    feat(supermaven): add support for supermaven-nvim (#633)
    ## Description
    
    Add `CmpItemKindSupermaven` support for plugin
    [supermaven-nvim](https://github.com/supermaven-inc/supermaven-nvim)
    like copilot and codeium.

--- extras/lua/tokyonight_storm.lua
+++ extras/lua/tokyonight_storm.lua
@@ -610,6 +610,10 @@ local highlights = {
   CmpItemKindSnippet = "LspKindSnippet",
   CmpItemKindString = "LspKindString",
   CmpItemKindStruct = "LspKindStruct",
+  CmpItemKindSupermaven = {
+    bg = "NONE",
+    fg = "#1abc9c"
+  },
   CmpItemKindTabNine = {
     bg = "NONE",
     fg = "#1abc9c"
