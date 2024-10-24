df3393b63e5808464f12cc12ad9d703c4b926f18
Juhan Ahmed <azurarobo280@gmail.com>
2024-10-24 16:34:41 +0600

    fix(termux): update termux theme to use the new terminal colors
    Refs: #648

--- lua/tokyonight/extra/termux.lua
+++ lua/tokyonight/extra/termux.lua
@@ -4,15 +4,6 @@ local M = {}
 
 --- @param colors ColorScheme
 function M.generate(colors)
-  local extended_colors = vim.tbl_extend("force", colors, {
-    red_bright = util.blend_fg(colors.red, 0.5),
-    green_bright = util.blend_fg(colors.green, 0.5),
-    yellow_bright = util.blend_fg(colors.yellow, 0.5),
-    blue_bright = util.blend_fg(colors.blue, 0.5),
-    magenta_bright = util.blend_fg(colors.magenta, 0.5),
-    cyan_bright = util.blend_fg(colors.cyan, 0.5),
-  })
-
   local termux = util.template(
     [[
 # -----------------------------------------------------------------------------
@@ -24,30 +15,30 @@ background: ${bg}
 foreground: ${fg}
 
 # Normal colors
-color0:  ${black}
-color1:  ${red}
-color2:  ${green}
-color3:  ${yellow}
-color4:  ${blue}
-color5:  ${magenta}
-color6:  ${cyan}
-color7:  ${fg_dark}
+color0:  ${terminal.black}
+color1:  ${terminal.red}
+color2:  ${terminal.green}
+color3:  ${terminal.yellow}
+color4:  ${terminal.blue}
+color5:  ${terminal.magenta}
+color6:  ${terminal.cyan}
+color7:  ${terminal.white}
 
 # Bright colors
-color8:  ${terminal_black}
-color9:  ${red_bright}
-color10: ${green_bright}
-color11: ${yellow_bright}
-color12: ${blue_bright}
-color13: ${magenta_bright}
-color14: ${cyan_bright}
-color15: ${fg}
+color8:  ${terminal.black_bright}
+color9:  ${terminal.red_bright}
+color10: ${terminal.green_bright}
+color11: ${terminal.yellow_bright}
+color12: ${terminal.blue_bright}
+color13: ${terminal.magenta_bright}
+color14: ${terminal.cyan_bright}
+color15: ${terminal.white_bright}
 
 # Extended colors
 color16: ${orange}
 color17: ${red1}
 ]],
-    extended_colors
+    colors
   )
 
   return termux
