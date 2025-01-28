0db70891ba33562f8658d43665a81fab40082c60
Folke Lemaitre <folke.lemaitre@gmail.com>
2025-01-29 00:14:42 +0100

    fix(snacks): EditWin => PickWin

--- lua/tokyonight/groups/snacks.lua
+++ lua/tokyonight/groups/snacks.lua
@@ -55,7 +55,7 @@ function M.get(c, opts)
     SnacksPickerBoxTitle         = { fg = c.orange, bg = c.bg_float },
     SnacksPickerSelected    = { fg = c.magenta2},
     SnacksPickerFlag         = "SnacksProfilerBadgeInfo",
-    SnacksPickerEditWin   = { fg = c.fg, bg = c.magenta2, bold = true },
+    SnacksPickerPickWin   = { fg = c.fg, bg = c.magenta2, bold = true },
   }
   for i, color in ipairs(c.rainbow) do
     ret["SnacksIndent" .. i] = { fg = color, nocombine = true }
