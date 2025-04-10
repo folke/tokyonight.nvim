c0831304cbd5b22839dc043bded05206a33bb88d
Lénaïc Terrier <dev@lenaicterrier.fr>
2025-04-11 00:10:25 +0200

    feat(extras): added qterminal theme

--- lua/tokyonight/extra/init.lua
+++ lua/tokyonight/extra/init.lua
@@ -26,6 +26,7 @@ M.extras = {
   lua              = { ext = "lua", url = "https://www.lua.org", label = "Lua Table for testing" },
   prism            = { ext = "js", url = "https://prismjs.com", label = "Prism" },
   process_compose  = { ext = "yaml", url = "https://f1bonacc1.github.io/process-compose/", label = "process-compose" },
+  qterminal        = { ext = "colorscheme", url = "https://github.com/lxqt/qterminal", label = "QTerminal" },
   slack            = { ext = "txt", url = "https://slack.com", label = "Slack" },
   sublime          = { ext = "tmTheme", url = "https://www.sublimetext.com/docs/themes", label = "Sublime Text" },
   spotify_player   = { ext = "toml", url = "https://github.com/aome510/spotify-player", label = "Spotify Player" },
