40f5afcf7c4155f50f13ff2ccd4b61ac7e66ed48
Josh <josh@joshthomas.dev>
2025-06-28 07:18:47 -0500

    feat(extra): add themes for opencode

--- lua/tokyonight/extra/init.lua
+++ lua/tokyonight/extra/init.lua
@@ -24,6 +24,7 @@ M.extras = {
   kitty            = { ext = "conf", url = "https://sw.kovidgoyal.net/kitty/conf.html", label = "Kitty" },
   lazygit          = { ext = "yml", url = "https://github.com/jesseduffield/lazygit", label = "Lazygit" },
   lua              = { ext = "lua", url = "https://www.lua.org", label = "Lua Table for testing" },
+  opencode         = { ext = "json", url = "https://github.com/sst/opencode", label = "opencode" },
   prism            = { ext = "js", url = "https://prismjs.com", label = "Prism" },
   process_compose  = { ext = "yaml", url = "https://f1bonacc1.github.io/process-compose/", label = "process-compose" },
   slack            = { ext = "txt", url = "https://slack.com", label = "Slack" },
