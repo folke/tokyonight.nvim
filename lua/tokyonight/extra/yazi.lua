7ba1d1c0da066b112bddcd40f3c4ddda17f2226a
Ojas Bhagavath <ojas.bhagavath@gmail.com>
2026-01-02 15:25:49 -0600

    fix(extras.yazi): fix broken yazi theme

--- lua/tokyonight/extra/yazi.lua
+++ lua/tokyonight/extra/yazi.lua
@@ -8,19 +8,19 @@ function M.generate(colors)
   colors.pmenusel = util.blend_bg(colors.fg_gutter, 0.8)
   local yazi = util.template(
     [[
+"$schema" = "https://yazi-rs.github.io/schemas/theme.json"
+[app]
+overall = { bg = "${bg_dark}"}
+
 [mgr]
-# NOTE: can be combined with tmTheme (sublime colorshceme file) for preview code highlight
-# syntect_theme = "path/to/tmTheme"
-
 cwd = { fg = "${fg_dark}", italic = true }
-
-# Hovered
-hovered         = { bg = "${bg_highlight}" }
-preview_hovered = { bg = "${bg_highlight}" }
 
 # Find
 find_keyword  = { fg = "${bg_dark}", bg = "${orange}", bold = true }
 find_position = { fg = "${info}", bg = "${search_bg}", bold = true }
+
+# Symlink
+symlink_target = {italic = true}
 
 # Marker
 marker_copied   = { fg = "${green}", bg = "${green}" }
@@ -37,6 +37,9 @@ count_selected = { fg = "${bg_dark}", bg = "${blue}" }
 border_symbol = "│"
 border_style  = { fg = "${border_highlight}" }
 
+# NOTE: can be combined with tmTheme (sublime colorshceme file) for preview code highlight
+# syntect_theme = "path/to/tmTheme"
+
 # Tab
 [tabs]
 active   = { fg = "${black}", bg = "${blue}" }
@@ -52,15 +55,16 @@ select_alt  = { fg = "${magenta}", bg = "${fg_gutter}" }
 unset_main  = { fg = "${black}", bg = "${purple}", bold = true }
 unset_alt   = { fg = "${purple}", bg = "${fg_gutter}" }
 
+# Hover
+[indicator]
+parent         = { bg = "${bg_highlight}" }
+current         = { bg = "${bg_highlight}" }
+preview = { bg = "${bg_highlight}" }
+
 [status]
 overall   = { fg = "${fg}", bg = "${bg_dark}" }
 sep_left  = { open = "", close = "" }
 sep_right = { open = "", close = "" }
-
-# Progress
-progress_label  = { fg = "${fg}", bold = true }
-progress_normal = { fg = "${blue0}", bg = "${bg_highlight}" }
-progress_error  = { fg = "${red1}", bg = "${bg_highlight}" }
 
 # Permissions
 perm_type  = { fg = "${blue}" }
@@ -69,6 +73,48 @@ perm_write = { fg = "${red}" }
 perm_exec  = { fg = "${green}" }
 perm_sep   = { fg = "${terminal_black}" }
 
+# Progress
+progress_label  = { fg = "${fg}", bold = true }
+progress_normal = { fg = "${blue0}", bg = "${bg_highlight}" }
+progress_error  = { fg = "${red1}", bg = "${bg_highlight}" }
+
+# Which
+[which]
+cols            = 3
+mask            = { bg = "${bg_dark}" }
+cand            = { fg = "${cyan}" }
+rest            = { fg = "${blue}" }
+desc            = { fg = "${magenta}" }
+separator       = " ➜ "
+separator_style = { fg = "${comment}" }
+
+# Confirm
+[confirm]
+border  = { fg = "${blue2}" }
+title   = { fg = "${border_highlight}" }
+body = {}
+list    = {}
+btn_yes = { bg = "${bg_visual}" }
+btn_no  = {}
+btn_labels = [ "  [Y]es  ", "  (N)o  " ]
+
+# Spottter
+[spot]
+border  = { fg = "${border_highlight}" }
+title   = { fg = "${border_highlight}" }
+
+# Notification
+[notify]
+title_info  = { fg = "${info}" }
+title_warn  = { fg = "${warning}" }
+title_error = { fg = "${error}" }
+
+# Icons
+icon_info = ""
+icon_warn = ""
+icon_error = ""
+
+# Picker
 [pick]
 border   = { fg = "${border_highlight}" }
 active   = { fg = "${fg}",  bg = "${bg_visual}" }
@@ -87,52 +133,18 @@ border   = { fg = "${blue2}" }
 active   = { fg = "${fg}", bg = "${pmenusel}" }
 inactive = { fg = "${fg}" }
 
+# Icons
 icon_file    = ""
 icon_folder  = ""
 icon_command = ""
 
-# Tasks
+# Task manager
 [tasks]
 border  = { fg = "${border_highlight}" }
 title   = { fg = "${border_highlight}" }
 hovered = { fg = "${fg}", bg = "${bg_visual}" }
 
-# Which
-[which]
-cols            = 3
-mask            = { bg = "${bg_dark}" }
-cand            = { fg = "${cyan}" }
-rest            = { fg = "${blue}" }
-desc            = { fg = "${magenta}" }
-separator       = " ➜ "
-separator_style = { fg = "${comment}" }
-
-# Confirm
-[confirm]
-border  = { fg = "${blue2}" }
-title   = { fg = "${border_highlight}" }
-content = {}
-list    = {}
-btn_yes = { bg = "${bg_visual}" }
-btn_no  = {}
-btn_labels = [ "  [Y]es  ", "  (N)o  " ]
-
-# Spot
-[spot]
-border  = { fg = "${border_highlight}" }
-title   = { fg = "${border_highlight}" }
-
-# Notify
-[notify]
-title_info  = { fg = "${info}" }
-title_warn  = { fg = "${warning}" }
-title_error = { fg = "${error}" }
-
-icon_error = ""
-icon_warn = ""
-icon_info = ""
-
-# Help
+# Help menu
 [help]
 on      = { fg = "${green}" }
 run     = { fg = "${magenta}" }
@@ -140,8 +152,8 @@ desc    = { fg = "${cyan}" }
 hovered = { bg = "${bg_highlight}" }
 footer  = { fg = "${fg}", bg = "${bg}" }
 
+# File-specific styles
 [filetype]
-
 rules = [
 	# Images
 	{ mime = "image/*", fg = "${yellow}" },
@@ -160,12 +172,12 @@ rules = [
 	# { mime = "inode/x-empty", fg = "${red}" },
 
 	# Special files
-	{ name = "*", is = "orphan", bg = "${red}" },
-	{ name = "*", is = "exec"  , fg = "${green}" },
+	{ url = "*", is = "orphan", bg = "${red}" },
+	{ url = "*", is = "exec"  , fg = "${green}" },
 
 	# Fallback
-	{ name = "*/", fg = "${blue}" },
-	{ name = "*", fg = "${fg}" }
+	{ url = "*/", fg = "${blue}" },
+	{ url = "*", fg = "${fg}" }
 ]
     ]],
     colors
